c60d159eb811aed0f32ef003d12cce4af0a3efe6
Ben <codeandcats@gmail.com>
2019-11-11 22:43:19 +1100

    feat(core): filter by movie Add optional options param to random quote function to allow filtering quotes by specific movies

--- packages/arnie-quote/src/index.test.ts
+++ packages/arnie-quote/src/index.test.ts
@@ -10,4 +10,34 @@ describe('getRandomQuote', () => {
     jest.spyOn(Math, 'random').mockReturnValue(0.9999);
     expect(getRandomQuote).toBeTruthy();
   });
+
+  it('should return a random quote from specific movies', () => {
+    jest.spyOn(Math, 'random').mockReturnValue(0.111);
+    expect(
+      getRandomQuote({
+        movies: ['Kindergarden Cop', 'Total Recall'],
+      }),
+    ).toEqual("It's not a tumor!");
+
+    jest.spyOn(Math, 'random').mockReturnValue(0.999);
+    expect(
+      getRandomQuote({
+        movies: ['Kindergarden Cop', 'Total Recall'],
+      }),
+    ).toEqual('Now get your ass to Mars');
+
+    jest.spyOn(Math, 'random').mockReturnValue(0.111);
+    expect(
+      getRandomQuote({
+        movies: ['Kindergarden Cop'],
+      }),
+    ).toEqual("It's not a tumor!");
+
+    jest.spyOn(Math, 'random').mockReturnValue(0.999);
+    expect(
+      getRandomQuote({
+        movies: ['Kindergarden Cop'],
+      }),
+    ).toEqual("It's not a tumor!");
+  });
 });
--- packages/arnie-quote/src/index.ts
+++ packages/arnie-quote/src/index.ts
@@ -1,19 +1,46 @@
-const quotes = [
-  "I'll be back",
-  'If it bleeds, we can kill it',
-  'Hasta la vista, baby',
-  'I need your clothes, your boots, and your motorcycle',
-  'Yeah, but they were all bad',
-  'It’s not a tumor!',
-  'You’re one ugly motherfucker.',
-  'Get to the choppa!',
-  "You're fired",
-  'Now get your ass to Mars',
+export const movies = [
+  'Kindergarden Cop',
+  'Predator',
+  'Terminator',
+  'Terminator 2',
+  'Total Recall',
+  'True Lies',
+] as const;
+
+export type Movie = typeof movies[number];
+
+export interface Quote {
+  value: string;
+  movie: Movie;
+}
+
+const quotes: Quote[] = [
+  { value: "I'll be back", movie: 'Terminator' },
+  { value: 'If it bleeds, we can kill it', movie: 'Predator' },
+  { value: 'Hasta la vista, baby', movie: 'Terminator 2' },
+  { value: 'I need your clothes, your boots, and your motorcycle', movie: 'Terminator 2' },
+  { value: 'Yeah, but they were all bad', movie: 'True Lies' },
+  { value: "It's not a tumor!", movie: 'Kindergarden Cop' },
+  { value: 'You’re one ugly motherfucker.', movie: 'Predator' },
+  { value: 'Get to the choppa!', movie: 'Predator' },
+  { value: "You're fired", movie: 'True Lies' },
+  { value: 'Now get your ass to Mars', movie: 'Total Recall' },
 ];
+
+export interface RandomQuoteOptions {
+  /**
+   * Limit quotes to these movies. If no movies specified will include all movies.
+   */
+  movies?: Movie[];
+}
 
 /**
  * Returns a random quote from Arnold Schwarzenegger.
  */
-const getRandomQuote = () => quotes[Math.trunc(Math.random() * quotes.length)];
+const getRandomQuote = (options: RandomQuoteOptions = {}) => {
+  const movies = (options && options.movies) || [];
+  const shortList = quotes.filter(quote => !movies.length || movies.includes(quote.movie));
+  return shortList[Math.trunc(Math.random() * shortList.length)].value;
+};
 
 export default getRandomQuote;
