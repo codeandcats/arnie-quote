ec4e1aa0c96238c2b5e8f181b2ad1dcb19ddc9bf
Ben <codeandcats@gmail.com>
2019-11-12 13:09:55 +1100

    ci(release): experimenting with release

--- packages/arnie-quote/support/prepareDistribution.ts
+++ packages/arnie-quote/support/prepareDistribution.ts
@@ -1,33 +1,18 @@
 #!/usr/bin/env -S npx ts-node
 
 import * as path from 'path';
-import { copyFileIfExistsSync, updateFileSync } from './common';
+import { updateFileSync } from './common';
 
-export function prepareDistribution() {
-  const rootPath = path.dirname(__dirname);
-  const distPath = path.join(rootPath, 'dist');
+const rootPath = path.dirname(__dirname);
+const distPath = path.join(rootPath, 'dist');
 
-  // Copy package.json into dist, then update it
-  const packageJsonFileName = path.join(rootPath, 'package.json');
-  const distPackageJsonFileName = path.join(distPath, 'package.json');
-  copyFileIfExistsSync(packageJsonFileName, distPackageJsonFileName);
-  updateFileSync(distPackageJsonFileName, content => {
-    const pkg = JSON.parse(content);
-    delete pkg.private;
-    delete pkg.scripts;
-    delete pkg.devDependencies;
-    pkg.main = pkg.main.replace(/^src\//, '').replace(/\.ts$/, '.js');
-    pkg.types = pkg.types.replace(/^src\//, '');
-    return JSON.stringify(pkg, null, '  ');
-  });
-
-  // Copy README into dist
-  copyFileIfExistsSync(path.join(rootPath, 'README.md'), path.join(distPath, 'README.md'));
-
-  // Copy CHANGE_LOG into dist
-  copyFileIfExistsSync(path.join(rootPath, 'CHANGELOG.md'), path.join(distPath, 'CHANGELOG.md'));
-}
-
-if (!module.parent) {
-  prepareDistribution();
-}
+// Strip out fields
+updateFileSync(path.join(distPath, 'package.json'), content => {
+  const pkg = JSON.parse(content);
+  delete pkg.private;
+  delete pkg.scripts;
+  delete pkg.devDependencies;
+  pkg.main = pkg.main.replace(/^src\//, '').replace(/\.ts$/, '.js');
+  pkg.types = pkg.types.replace(/^src\//, '');
+  return JSON.stringify(pkg, null, '  ');
+});
